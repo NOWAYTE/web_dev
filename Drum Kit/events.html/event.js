44135d86a9a2f58e494aa22ffade151b3e9e93e6
Daniel kamau <danielkamaunganga3@gmail.com>
2023-05-26 19:44:40 +0300

    Advanced javascript

---
+++ Drum Kit/events.html/event.js
@@ -0,0 +1,21 @@
+
+var header = document.querySelector("#headerText")
+
+function change(){
+    alert("wassup doofus");
+}
+
+// document.addEventListener("click", function change(){
+//     alert("Dont you dare click me  :) !");
+// });
+
+// document.addEventListener("", function () {
+//     header.style.color = "red";
+
+// });
+
+/**document.addEventListener("click", function(){
+    header.style.color = "red";
+
+    header.textContent = "Hello my name is Daniel ";
+})*/
---
+++ Drum Kit/index.js
@@ -0,0 +1,84 @@
+
+//detecting click on the screen 
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++)
+    {
+        document.querySelectorAll("button")[i].addEventListener("click", function()
+        {
+            var button  = this.innerHTML;
+
+            makeSound(button);
+
+            buttonAnimation(button)
+           
+        });
+    }
+
+    //detecting button click 
+
+    document.addEventListener("keypress", function(event){
+        makeSound(event.key);
+
+        buttonAnimation(event.key);
+    });
+
+    function makeSound(key)
+    {
+        switch (key) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+           
+            default:
+                console.log("button");
+                break;
+           }
+
+      
+    }
+
+    function buttonAnimation(currentKey){
+
+        var pressedButton = document.querySelector("." + currentKey);
+
+        pressedButton.classList.add("pressed");
+
+        setTimeout(function(){
+            pressedButton.classList.remove("pressed")
+        }, 100);
+    }
+
+
+

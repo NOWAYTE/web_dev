54173d9a0ea5df1e54eb62b2cd5b1bb3425fa094
Daniel kamau <danielkamaunganga3@gmail.com>
2023-07-04 12:18:58 +0300

    A simple weather app

---
+++ API/weatherProject/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const https = require('https')
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.htm')
+
+  app.post('/', function (req, res) {
+    console.log(req.body.cityName)
+
+    const query = req.body.cityName;
+    const apiKey = '8d5e0fe3c73511005b2c15aa0b162729'
+    const units = 'metric'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&appid=${apiKey}`
+    https.get(url, function (response) {
+      console.log(response.statusCode)
+
+      response.on('data', (data) => {
+        const weatherData = JSON.parse(data)
+        const temp = weatherData.main.temp
+        const description = weatherData.weather[0].description
+        const icon = weatherData.weather[0].icon
+        const img = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+
+        res.write(`<h1>The weather temp in  ${query} is ${temp} degrees celcius<h1>`)
+        res.write(`<h2><br>the weather is currenlty ${description}<h2>`)
+        res.write(`<img src = ${img} >`)
+        res.send()
+      })
+    })
+  })
+})
+
+app.listen('3000', () => {
+  console.log('server is running on port : 3000')
+})
